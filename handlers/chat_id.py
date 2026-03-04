45f660e1a46177aa32d5b16f4870864548f434d3
wolski <wolski@MacBook-Pro---Pavel-Wolski.local>
2026-03-04 12:06:44 +0200

    Add Docker and CI/CD workflow

--- handlers/chat_id.py
+++ handlers/chat_id.py
@@ -1,9 +1,14 @@
 from aiogram.types import Message
 from aiogram import Router
 from aiogram.filters import Command
+from database import load_allowed_users
 
 router = Router()
 
 @router.message(Command("start"))
 async def cmd_start(message: Message):
+    allowed_users = load_allowed_users()
+    if message.from_user.id not in allowed_users:
+        await message.answer("❌ У вас немає доступу до боту")
+        return
     await message.answer(f"Chat ID: {message.chat.id}")
